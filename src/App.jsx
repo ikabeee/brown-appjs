762382913aac6f730bfaa231fa8d8f57a8378152
ikabeee <carlglz30@gmail.com>
2025-01-15 01:22:09 -0500

    Add layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { Route, Routes } from "react-router";
+
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Login from "./pages/Login/Login"
+import { ComplexNavbar } from "./layouts/ComplexNavbar";
+
 
 function App() {
 
   return (    
     <>
+      <Routes>
+        <Route element={<ComplexNavbar/>} >
+          <Route index element={<Dashboard/>} />
+          <Route path="login" element={<Login/>} />
+        </Route>
+      </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router'
+import { ThemeProvider } from '@material-tailwind/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
