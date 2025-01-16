2505eaa32f541a5f80bdeef2be905d9ff214bddf
SebasEPV <sperezvinas@gmaill.com>
2025-01-16 11:37:26 -0500

    Added BarChart Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
-import LineChart from "./components/LineChart";
-import BarChart from "./components/BarChart";
+
 
 
 
@@ -9,7 +8,6 @@ function App() {
 
   return (    
     <>
-    <BarChart/>
     </>
   )
 }
